21fc0f053499b61691d5295a8e396e37e8328779
dspain <dmspain@yahoo.com>
2018-05-12 20:26:16 -0700

    add useful worker functions

--- metex_otp/lib/worker.ex
+++ metex_otp/lib/worker.ex
@@ -27,4 +27,44 @@ defmodule MetexOtp.Worker do
   end
 
   ## Helper functions
+
+  def temperature_of(location) do
+    url_for(location) |> HTTPoison.get() |> parse_response
+  end
+
+  defp url_for(location) do
+    "http://api.openweathermap.org/data/2.5/weather?q=#{location}&APPID=#{apikey}"
+  end
+
+  defp parse_response({:ok, %HTTPoison.Response{body: body, status_code: 200}}) do
+    body |> JSON.decode!() |> compute_temperature
+  end
+
+  defp parse_response(_) do
+    :error
+  end
+
+  defp compute_temperature(json) do
+    try do
+      temp = (json["main"]["temp"] - 273.15) |> Float.round(1)
+      {:ok, temp}
+    rescue
+      () ->
+        :error
+    end
+  end
+
+  defp apikey do
+    "d6c7024fb622df18a792f90365917df8"
+  end
+
+  defp update_stats(old_stats, location) do
+    case Map.has_key?(old_stats, location) do
+      true ->
+        Map.update!(old_stats, location, &(&1 + 1))
+
+      false ->
+        Map.put_new(old_stats, location, 1)
+    end
+  end
 end
