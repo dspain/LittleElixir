ac9198f62f85e3977f7c4b045a73f658ed28bd25
dspain <dmspain@yahoo.com>
2018-05-12 22:41:59 -0700

    read elixir term using key

--- metex_otp/lib/cache.ex
+++ metex_otp/lib/cache.ex
@@ -11,6 +11,10 @@ defmodule MetexOtp.Cache do
     GenServer.cast(@name, {:write, {key, term}})
   end
 
+  def read(key) do
+    GenServer.call(@name, {:read, key})
+  end
+
   ## Server callbacks
   def init(:ok) do
     {:ok, %{}}
@@ -18,6 +22,10 @@ defmodule MetexOtp.Cache do
 
   def handle_cast({:write, {key, term}}, _from, state) do
     {:reply, {key, term}, state}
+  end
+
+  def handle_call({:read, key, _from, state}) do
+    {:reply, state[key], state}
   end
 
   ## Helper functions
