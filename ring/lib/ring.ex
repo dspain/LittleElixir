dfa4b611cf7bfc403f84c14ad8e785867c4f10dc
dspain <dmspain@yahoo.com>
2018-05-13 20:46:06 -0700

    add parens to function call

--- ring/lib/ring.ex
+++ ring/lib/ring.ex
@@ -1,13 +1,13 @@
 defmodule Ring do
   def create_processes(n) do
-    1..n |> Enum.map(fn _ -> spawn(fn -> loop end) end)
+    1..n |> Enum.map(fn _ -> spawn(fn -> loop() end) end)
   end
 
   def loop do
     receive do
       {:link, link_to} when is_pid(link_to) ->
         Process.link(link_to)
-        loop
+        loop()
 
       :crash ->
         1 / 0
