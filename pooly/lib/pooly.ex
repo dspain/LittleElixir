f2eceeb238a0a2486e94c379e4d1c2c068b6003c
dspain <dmspain@yahoo.com>
2018-05-19 12:08:58 -0700

    flush out the init callback

--- pooly/lib/pooly.ex
+++ pooly/lib/pooly.ex
@@ -14,7 +14,10 @@ defmodule Pooly.WorkerSupervisor do
   #############
   # Callbacks #
   #############
-  def init({m, f, a}) do
-    # ...
+  def init({m, f, a} = x) do
+    worker_opts = [restart: :permanent, function: f]
+    children = [worker(m, a, worker_opts)]
+    opts = [strategy: :simple_one_for_one, max_restarts: 5, max_seconds: 5]
+    supervise(children, opts)
   end
 end
