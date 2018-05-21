f949ba76eb3f3bf812fa75b6e85905dcef31e0c5
dspain <dmspain@yahoo.com>
2018-05-20 21:48:22 -0700

    unused x

--- pooly/lib/worker_supervisor.ex
+++ pooly/lib/worker_supervisor.ex
@@ -11,7 +11,7 @@ defmodule Pooly.WorkerSupervisor do
   #############
   # Callbacks #
   #############
-  def init({m, f, a} = x) do
+  def init({m, f, a}) do
     worker_opts = [restart: :permanent, function: f]
     children = [worker(m, a, worker_opts)]
     opts = [strategy: :simple_one_for_one, max_restarts: 5, max_seconds: 5]
