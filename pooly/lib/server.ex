60fdff85ab3bbd274617b849172896210f90be75
dspain <dmspain@yahoo.com>
2018-05-20 21:41:33 -0700

    provide info about the number of checked out workers

--- pooly/lib/server.ex
+++ pooly/lib/server.ex
@@ -21,6 +21,10 @@ defmodule Pooly.Server do
     GenServer.cast(__MODULE__, {:checkin, worker_pid})
   end
 
+  def status do
+    GenServer.call(__MODULE__, :status)
+  end
+
   #############
   # Callbacks #
   #############
@@ -74,6 +78,10 @@ defmodule Pooly.Server do
       [] ->
         {:noreply, state}
     end
+  end
+
+  def handle_call(:status, _from, %{workers: workers, monitors: monitors} = state) do
+    {:reply, {length(workers), :ets.info(monitors, :size)}, state}
   end
 
   #####################
