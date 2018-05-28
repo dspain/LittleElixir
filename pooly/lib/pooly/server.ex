79451e0864e261d481d9f6b709bf767bdef2a6c8
dspain <dmspain@yahoo.com>
2018-05-27 22:15:47 -0700

    implement the Poolboy transaction feature

--- pooly/lib/pooly/server.ex
+++ pooly/lib/pooly/server.ex
@@ -21,6 +21,16 @@ defmodule Pooly.Server do
     Pooly.PoolServer.status(pool_name)
   end
 
+  def transaction(pool_name, fun, timeout) do
+    worker = checkout(pool_name, true, timeout)
+
+    try do
+      fun.(worker)
+    after
+      checkin(pool_name, worker)
+    end
+  end
+
   #############
   # Callbacks #
   #############
