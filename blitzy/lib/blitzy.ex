988d5de4fdc98e8a37c616f2c49a6ef16eca0339
dspain <dmspain@yahoo.com>
2018-05-28 22:04:59 -0700

    need to pass caller to start

--- blitzy/lib/blitzy.ex
+++ blitzy/lib/blitzy.ex
@@ -1,6 +1,6 @@
 defmodule Blitzy do
   def run(n_workers, url) when n_workers > 0 do
-    worker_fun = fn -> Blitzy.Worker.start(url) end
+    worker_fun = fn -> Blitzy.Worker.start(url, self()) end
 
     1..n_workers
     |> Enum.map(fn _ -> Task.async(worker_fun) end)
