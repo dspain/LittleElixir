6ee809efbd99467efadcf8a02e2f85e76831789a
dspain <dmspain@yahoo.com>
2018-05-28 21:56:52 -0700

    modify worker to send its results to caller

--- blitzy/lib/blitzy/worker.ex
+++ blitzy/lib/blitzy/worker.ex
@@ -2,9 +2,11 @@ defmodule Blitzy.Worker do
   use Timex
   require Logger
 
-  def start(url) do
-    {timestamp, response} = Duration.measure(fn -> HTTPoison.get(url) end)
-    handle_response({Duration.to_milliseconds(timestamp), response})
+  def start(url, caller, func \\ &HTTPoison.get/1) do
+    {timestamp, response} = Duration.measure(fn -> func.(url) end)
+
+    caller
+    |> send({self, handle_response({Duration.to_milliseconds(timestamp), response})})
   end
 
   defp handle_response({msecs, {:ok, %HTTPoison.Response{status_code: code}}})
