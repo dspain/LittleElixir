f6a775bca03631d53673e26a906cf6c56902906c
dspain <dmspain@yahoo.com>
2018-05-28 22:06:01 -0700

    let task wait forever

--- blitzy/lib/blitzy.ex
+++ blitzy/lib/blitzy.ex
@@ -4,6 +4,6 @@ defmodule Blitzy do
 
     1..n_workers
     |> Enum.map(fn _ -> Task.async(worker_fun) end)
-    |> Enum.map(&Task.await(&1))
+    |> Enum.map(&Task.await(&1, :infinity))
   end
 end
