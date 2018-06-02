1a546e600f0fdd74286b8663e30bdc2303765e9f
dspain <dmspain@yahoo.com>
2018-06-01 22:39:36 -0700

    implement the application behavior

--- chucky/lib/chucky.ex
+++ chucky/lib/chucky.ex
@@ -1,18 +1,30 @@
 defmodule Chucky do
-  @moduledoc """
-  Documentation for Chucky.
-  """
+  use Application
+  require Logger
 
-  @doc """
-  Hello world.
+  def start(type, _args) do
+    import Supervisor.Spec
 
-  ## Examples
+    children = [
+      worker(Chucky.Server, [])
+    ]
 
-      iex> Chucky.hello
-      :world
+    case type do
+      :normal ->
+        Logger.info("Application is started on #{node}")
 
-  """
-  def hello do
-    :world
+      {:takeover, old_node} ->
+        Logger.info("#{node} is taking over #{old_node}")
+
+      {:failover, old_node} ->
+        Logger.info("#{old_node} is failing over to #{node}")
+    end
+
+    opts = [strategy: :one_for_one, name: {:global, Chucky.Supervisor}]
+    Supervisor.start_link(children, opts)
+  end
+
+  def fact do
+    Chucky.Server.fact()
   end
 end
