e389ab1aede315869469e9df09ad4b589e1392e1
dspain <dmspain@yahoo.com>
2018-06-05 21:43:51 -0700

    encoding is the reverse of decoding

--- quickcheck_playground/test/lists_eqc.exs
+++ quickcheck_playground/test/lists_eqc.exs
@@ -7,4 +7,10 @@ defmodule ListsEQC do
       ensure(l |> Enum.reverse() |> Enum.reverse() == l)
     end
   end
+
+  property "encoding is the reverse of decoding" do
+    forall bin <- binary do
+      ensure(bin |> Base.encode64() |> Base.decode64!() == bin)
+    end
+  end
 end
