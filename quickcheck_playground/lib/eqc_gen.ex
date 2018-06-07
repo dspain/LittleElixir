7d45545a26a41cd6bfbb63ccf65991ec82afaf55
dspain <dmspain@yahoo.com>
2018-06-06 21:25:21 -0700

    create strings with variable length

--- quickcheck_playground/lib/eqc_gen.ex
+++ quickcheck_playground/lib/eqc_gen.ex
@@ -2,6 +2,12 @@ defmodule EQCGen do
   use EQC.ExUnit
 
   def string_with_fixed_length(len) do
-    vector(len, char)
+    vector(len, char())
+  end
+
+  def string_with_variable_length do
+    let len <- choose(1, 10) do
+      vector(len, char())
+    end
   end
 end
