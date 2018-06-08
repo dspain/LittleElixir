0984b6b533daa517efdd7eaaaab2ffc10a2001f3
dspain <dmspain@yahoo.com>
2018-06-07 21:42:06 -0700

    create a nested list generator

--- quickcheck_playground/lib/eqc_gen.ex
+++ quickcheck_playground/lib/eqc_gen.ex
@@ -16,4 +16,18 @@ defmodule EQCGen do
       vector(len, frequency([{3, oneof(:lists.seq(?a, ?z))}, {1, ?,}]))
     end
   end
+
+  def nested_list(gen) do
+    sized size do
+      nested_list(size, gen)
+    end
+  end
+
+  defp nested_list(0, _gen) do
+    []
+  end
+
+  defp nested_list(n, gen) do
+    oneof([[gen | nested_list(n - 1, gen)], [nested_list(n - 1), gen]])
+  end
 end
